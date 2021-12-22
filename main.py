2f74a263bf3c8e2a28c903573e71fc6e907633fa
heryanto <heryanto@kali>
2021-12-22 15:35:31 +0800

    menambahkan perkalian

--- main.py
+++ main.py
@@ -2,32 +2,17 @@ def penambahan(a,b):
 	total = a+b
 	return total
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-def pengurangan(a,b):
-	total = a-b
-=======
-=======
 def pengurangan(a,b):
 	total = a-b
 	return total
 
->>>>>>> perkalian
 def perkalian(a,b):
 	total = a*b
->>>>>>> perkalian
 	return total
 
 def main():
 	print(penambahan(10,5))
-<<<<<<< HEAD
-<<<<<<< HEAD
 	print(pengurangan(10,5))
-=======
-=======
-	print(pengurangan(10,5))
->>>>>>> perkalian
 	print(perkalian(10,5))
->>>>>>> perkalian
 
 main()
